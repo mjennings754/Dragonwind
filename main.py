ac70e126df2f4b114ef4d50e2da5a442f75d4a11
mjennings754 <mitchellj@outlook.jp>
2026-01-30 08:09:27 +1100

    add Dungeoneer class for player

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 title = pygame.display.set_caption("Dragonwind")
 clock = pygame.time.Clock()
-x = 200
-y = 200
-scale = 3
-player = pygame.image.load('images/player/idle.png').convert()
-player = pygame.transform.scale(player, (player.get_width() * scale, player.get_height() * scale))
-rect = player.get_rect()
-rect.center = (x, y)
+
+class Dungeoneer(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('images/player/idle.png').convert()
+        self.image = pygame.transform.scale(img, (img.get_width() * scale, img.get_height() * scale))
+        self.rect = img.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+player = Dungeoneer(100, 100, 3)
 
 FPS = 60
 running = True
 while running:
-    screen.blit(player, rect)
+    player.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
