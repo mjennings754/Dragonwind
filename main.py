2e26f1ce2016601e310ec2e391a8cb5dbf015090
mjennings754 <mitchellj@outlook.jp>
2026-01-29 15:34:21 +1100

    can use escape

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
 pygame.quit()
