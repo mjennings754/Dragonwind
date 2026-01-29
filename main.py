12f00a5c51ae196fc1ff770281a299343dafd98b
mjennings754 <mitchellj@outlook.jp>
2026-01-29 15:45:02 +1100

    add FPS

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 title = pygame.display.set_caption("Dragonwind")
+clock = pygame.time.Clock()
+FPS = 60
 running = True
 while running:
     for event in pygame.event.get():
@@ -11,5 +13,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+    clock.tick(FPS)
 
 pygame.quit()
