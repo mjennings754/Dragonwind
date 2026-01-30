94ddd38e5a8cd9e7b9abcc80a081903c66396f0b
mjennings754 <mitchellj@outlook.jp>
2026-01-30 12:15:23 +1100

    add jumping

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ clock = pygame.time.Clock()
 moving_left = False
 moving_right = False
 BG_COLOR = (0, 124, 124)
+LINE_COLOR = (255, 0, 0)
+FPS = 60
+
+running = True
+
+GRAVITY = 0.55
+
 
 def set_background():
     screen.fill(BG_COLOR)
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (1280, 400))
 
 class Dungeoneer(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
@@ -21,7 +29,9 @@ class Dungeoneer(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
         self.flip = False
+        self.jump = False
 
     def move(self, moving_left, moving_right):
         dx = 0
@@ -36,6 +46,19 @@ class Dungeoneer(pygame.sprite.Sprite):
             self.flip = False
             self.direction = 1
 
+        if self.jump == True:
+            self.vel_y = -9
+            self.jump = False
+
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+
+
         self.rect.x += dx
         self.rect.y += dy
 
@@ -44,8 +67,6 @@ class Dungeoneer(pygame.sprite.Sprite):
 
 player = Dungeoneer(100, 100, 3, 5)
 
-FPS = 60
-running = True
 while running:
     set_background()
     player.draw()
@@ -60,6 +81,8 @@ while running:
                 moving_right = True
             if event.key == pygame.K_a:
                 moving_left = True
+            if event.key == pygame.K_SPACE:
+                player.jump = True
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
